9bf1d19fad041a69169616b6010b3b977038c6a5
Craig Snow <acsnow@gmail.com>
2021-05-13 19:38:57 +0000

    updated bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/craigsnow-training/s3-bucket/aws"
-  version = "2.1.0"
+  version = "1.15.0"
   bucket_prefix = "craigsnow"
   # insert required variables here
 }
